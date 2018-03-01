e3b0004aeee406bf76f1bcc5badc01246e096307
fgabel <kpazrael2@gmail.com>
2018-03-01 18:19:48 +0100

    changes to the template
    created an abstract model class "DLRTnet"

---
+++ model/dlrt.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+# python 3 confusing imports :(
+from .unet_parts import *
+
+
+
+
+
+class DLRTnet(nn.Module):
+    """DLRTnet is based on https://arxiv.org/abs/1701.08936
+    # write something about the architecture
+    
+    """
+    def __init__(self, *args):
+        """args:
+            - a
+            - b
+            - c 
+        """
+        
+    @staticmethod
+    def weight_init(m):
+        """ call this for weight initialisation 
+        """
+        if isinstance(m, nn.Conv2d):
+            init.xavier_normal(m.weight)
+            init.constant(m.bias, 0)
+
+
+    def reset_params(self):
+        """ call this for weight reset in each of the modules
+        # we might not need this
+        """
+        for i, m in enumerate(self.modules()):
+            self.weight_init(m)
+
+
+    def forward(self, x):
+
+        return x
