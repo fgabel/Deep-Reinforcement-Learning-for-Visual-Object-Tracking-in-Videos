5cd48dfb6526a96ff1f215e8937131b1817c89ee
3inmal <bundgaardnils@gmail.com>
2018-03-08 15:50:24 +0100

    Update DataLoader.py

--- dataset/DataLoader.py
+++ dataset/DataLoader.py
@@ -23,12 +23,11 @@ import matplotlib.pyplot as plt
 import matplotlib.patches
 import time
 import os
+import glob
 import copy
 from PIL import Image # import jpg in python
 from skimage.io import imread_collection, imread, concatenate_images # import all images from a folder, see the dataloader
-
-
-
+import shapely
 
 #%%
 
@@ -42,16 +41,34 @@ print( vid_list[0][33])
 n = vid_list.shape[0]
 
 
+#%% Test
+
+test =os.path.join('F:/vot2017/',
+                   vid_list.iloc[0,0])
+
+included_extension = ['jpg']
+file_names = [fn for fn in os.listdir(test)
+              if any(fn.endswith(ext) for ext in included_extension)]
+#%% Is rectangular 
+
+#for i in range(vid_list.shape[0]):
+    gt = pd.read_csv(os.path.join('F:/vot2017/', 
+                              vid_list.iloc[3,0],
+                              'groundtruth.txt'), header = None)
+    gt = gt.values
+    Xs = gt[:,::2] # Save Xcoords
+    Ys = gt[:,1::2] # Save Ycoords
+    print(Xs)
 #%% Test drawing
 
 # Load the gt boxes, which are apparently stored in coordinates of 4 points
-gt = pd.read_csv('F:/vot2017/ants1/groundtruth.txt', header = None)
-
+gt = pd.read_csv('F:/vot2017/ball1/groundtruth.txt', header = None)
+gt = gt.values
 
 # Transform dataset into array
 # Not neccessary dt.iloc[x] does it
 
-#im  = Image.open('F:/vot2017/ants1/00000092.jpg')
+im  = Image.open('F:/vot2017/ball1/00000095.jpg')
 
 # Draws a rectangle given the coordinates of all four corners in one array
 # Where the order is upper-left, upper-right, lower-rigth, lower-left
@@ -71,7 +88,7 @@ def draw_gt(im, coords):
     plt.show()
 
 
-#draw_gt(im, gt.iloc[91])
+draw_gt(im, gt[94])
 #Check
 
 
@@ -87,35 +104,41 @@ class VOT2017_dataset(Dataset):
             csv_file(string): Path to list file, where all videos are listed
             root_dir(string): Directory with all the videos
             transform(callable, optional): Will transform on a sample(for pytorch I guess)
+            
         """
         self.vot_list = pd.read_csv(csv_file, header = None)
         self.root_dir = root_dir
         self.transform = transform
     
-    # Returns the nr of videos available
-    def __len__(self):
-        return len(self.vot_list)
     
     # Return the complete video sequence
-    def __getitem__(self, vid_idx):
+    def __getitem__(self, vid_idx, T = 10):
         """ Arguments:
             vid_idx(int): Video Index to be fetched form the video list
+            T(int): Nr of Images in sequence - default == 10
         """
-        vid_name_path = os.path.join(self.root_dir, 
-                                     self.vot_list.iloc[vid_idx,0],
-                                     '*.jpg')
-        
         gt = pd.read_csv(os.path.join(self.root_dir, 
                                       self.vot_list.iloc[vid_idx,0],
                                       'groundtruth.txt'), header = None)
         
-        im_seq = imread_collection(vid_name_path)
+        vid_name_path = os.path.join(self.root_dir, 
+                                     self.vot_list.iloc[vid_idx,0],
+                                     '*.jpg')
+        
+        file_names = glob.glob(vid_name_path)
+        
+        rand_start = np.random.randint(0, len(file_names)-T+1)
+        
+        file_names = file_names[rand_start:(rand_start+T-1)]
+        
+        im_seq = imread_collection(file_names)
         
         # Image collection to np.array
         images = concatenate_images(im_seq) # Shape(Nr. of images, h, w, RGB)
         
         # Also convert the gt to np.array
         gt = gt.values
+        gt = gt[rand_start:(rand_start+T-1),:]
         
         sample = {'Video': images, 'gt': gt}
         
@@ -130,11 +153,37 @@ class VOT2017_dataset(Dataset):
 test = VOT2017_dataset(csv_file= 'F:/vot2017/list.txt',
                        root_dir= 'F:/vot2017/')  
 
-# E.g. load the second video of the vid_list
-sample = test[2]
+# E.g. load a of the vid_list
+sample = test.__getitem__(0, T = 20)
 
 # Simply draw a single video - here the idx refers to the image in the sequence
-draw_gt(sample['Video'][0], sample['gt'][0])
+draw_gt(sample['Video'][10], sample['gt'][10])
+
+#%% define loss/reward functions; given the coordinates of all corners
+
+def loss_v1(pred, gt):
+    r = - np.mean(np.absolute(pred-gt)) - np.max(np.absolute(pred-gt))
+    return r
+
+
+# Calculate the reward given all the for corners x1,y1,x2,y2,x3,y3,x4,y4
+def loss_v2(pred, gt):
+    #reorder the coord in tuples for the polygon
+    pred_re = [(pred[0],pred[1]),(pred[2],pred[3]), (pred[4],pred[5]),(pred[6],pred[7])]
+    gt_re   = [(gt[0],gt[1]),(gt[2],gt[3]),(gt[4],gt[5]),(gt[6],gt[7])]
+    
+    pred_poly = Polygon(pred_re)
+    gt_poly = Polygon(gt_re)
+    # Reward == Intersection/total area
+    r = pred_poly.intersection(gt_poly).area/(pred_poly.area + gt_poly.area - pred_poly.intersection(gt_poly).area)
+    return r
+#%% Test reward functions
+    
+test_1 = np.array((0,0,0,1,1,1,1,0))
+test_2 = np.array((0.5,0,0.5,1,1.5,1,1.5,0))
+
+print(loss_v1(test_1, test_2))
+print(loss_v2(test_1, test_2))
 
 
 #%% Just for information - Find the smallest sized video
@@ -145,7 +194,7 @@ Size = np.zeros((Vids, 2))
 
 for i in range(Vids):
     im = Image.open(os.path.join('F:/vot2017/', 
-                                     vid_list.iloc[i,0],
+                                    vid_list.iloc[i,0],
                                      '00000001.jpg'))
     Size[i,0] = im.size[0]
     Size[i,1] = im.size[1]
@@ -233,8 +282,11 @@ class ToTensor(object):
                 'gt': torch.from_numpy(gt)}
         
 #%% Test the ToTensor
-        
+
+
+rescale = Rescale()
+rescale(sample)
 tens = ToTensor()
 tens(sample)
 
-# Still have to test this
+# Apparently not enoguh RAM on my machine here to fully check that it works :D
